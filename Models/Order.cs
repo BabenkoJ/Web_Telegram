fa519e468e59e607cddbad2e00fefb7b6b81f55b
Julia <Julia@LAPTOP-5DK3M7NC>
2021-11-25 22:34:53 +0400

    Добавьте файлы проекта.

---
+++ Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Telegram.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string name { get; set; } // имя фамилия 
+        public string email { get; set; } // email
+        public string msg { get; set; } // сообщение
+    }
+}
